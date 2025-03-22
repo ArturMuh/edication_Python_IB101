8081e311f62538f91e2c24657d7307ae82844ee8
ArturMuh <97883077+ArturMuh@users.noreply.github.com>
2025-03-22 11:57:54 +0500

    Add files via upload

---
+++ 09/01.py
@@ -0,0 +1,5 @@
+text = input()
+if len(text) >= 5:
+    print('ДА')
+else:
+    print('НЕТ')
---
+++ 09/main.py
@@ -0,0 +1,26 @@
+#  Строки.Срезы
+
+# ticket = '153153' #input()
+# odd = 0
+# even = 0
+# for ix, num in enumerate(ticket):
+#     if ix % 2 == 0:
+#         odd += int(num)
+#     else:
+#         even += int(num)
+# if odd == even:
+#     print('Lucky')
+# else:
+#     print('Oh no ((')
+
+# # Срезы строк
+# ticket = '153153' #input()
+# print(ticket[1:len(ticket):2])
+
+# text = 'Hello'
+# print(text[:1000:])
+
+# text = 'СЕЛ В ОЗЕРЕ БЕРЕЗОВ ЛЕС'
+# text_reversed = text[::-1]
+# print(text == text_reversed)
+
